d7487a092cb68a66b116ae12d277a5791e42ffa9
thrall31 <victor.roux@hotmail.fr>
2016-05-13 17:39:15 +0200

    Add files via upload

--- TestsAutoEcole.sql
+++ TestsAutoEcole.sql
@@ -21,7 +21,14 @@ exec autoecole.ajoutVehicule('124587789', 'Citroën', 'C3');
 exec autoecole.ajoutSalarie('Castafolt', 'Philippe', 'Riton', '24 rue de la blague','Rodez', '12000', '0654879535', 1, 1);
 exec autoEcole.ajoutClient('ROUX', 'André', '12 avenue du bonheur', 'Rodez', '12000', '0525854696', '15/07/1967');
 exec autoEcole.ajoutEleve('ROUX', 'Victor', '12 avenue du bonheur', 'Rodez', '12000', '0654875241', '15/06/1990', 1, 1);
-exec autoEcole.sommeLeconEleve(1);
-exec autoEcole.dateCodeEleve(1);
+exec autoEcole.ajoutEleve('ROUX', 'Francky', '12 avenue du bonheur', 'Rodez', '12000', '0625487936', '15/02/1995', 1, 1);
+exec autoEcole.ajoutLecon('02/04/2016', 1, 1, 1, 2);
+exec autoEcole.ajoutLecon('12/04/2016', 1, 1, 1, 2);
+exec autoEcole.ajoutExamen('12/04/2016', 1);
+exec autoEcole.ajoutPasser(1, '12/04/2016', 1, 0);
+
+SELECT autoecole.sommeLeconEleve(1) FROM dual;
+SELECT autoEcole.dateCodeEleve(1) FROM dual;
+SELECT autoEcole.sommeAchatClient(1, null) from dual; 
 
 
